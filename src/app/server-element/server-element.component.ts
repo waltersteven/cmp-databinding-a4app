9bf50e576d8588f70fad517c7c8b5a36e5cde2af
Walter Pariona <wltrst97@gmail.com>
2017-11-03 14:40:01 -0500

    Lifecycle hooks in action

--- src/app/server-element/server-element.component.ts
+++ src/app/server-element/server-element.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input, ViewEncapsulation } from '@angular/core';
+import { Component, OnInit, Input, ViewEncapsulation, OnChanges, SimpleChanges, DoCheck, AfterContentInit, AfterContentChecked, AfterViewInit, AfterViewChecked, OnDestroy } from '@angular/core';
 
 @Component({
   selector: 'app-server-element',
@@ -6,14 +6,46 @@ import { Component, OnInit, Input, ViewEncapsulation } from '@angular/core';
   styleUrls: ['./server-element.component.css'],
   encapsulation: ViewEncapsulation.Emulated //To expose our style to every component, Emulated is the default, alternatives: None, Native.
 })
-export class ServerElementComponent implements OnInit {
+export class ServerElementComponent implements OnInit, OnChanges, DoCheck, AfterContentInit, AfterContentChecked, AfterViewInit, AfterViewChecked, OnDestroy {
   //With Input we expose the element property to the others components in order to RECEIVE data from app.component.html. 
   //The string passed works as an alias, now you have to use it, 'element' will no longer work.
-  @Input('srvElement') element: {type: string, name: string, content: string}; //Javascript object.
+  @Input('srvElement') element:  {type: string, name: string, content: string}; //Javascript object.
+  @Input() name: string;
+  constructor() {
+    console.log('constructor called!');
+   }
+  
+  ngOnChanges(changes: SimpleChanges){ //Called after a bound input property changed.
+    console.log('ngOnChanges called!');
+    console.log(changes);    
+  }
 
-  constructor() { }
+  ngOnInit() { //Called once the component is initialized.
+    console.log('ngOnInit called!');    
+  }
 
-  ngOnInit() {
+  ngDoCheck(){ //Called every change detection run
+    console.log('ngDoCheck called!');    
+  }
+
+  ngAfterContentInit(){ //Called after ng-content has been projected into view.
+    console.log('ngAfterContentIinit called!');    
+  }
+
+  ngAfterContentChecked(){ //Called everytime the projected content has been checked.
+    console.log('ngAfterContentChecked called!');    
+  }
+
+  ngAfterViewInit(){ //Called after the component's view has been initialized.
+    console.log('ngAfterViewInit called!');    
+  }
+
+  ngAfterViewChecked(){ //Called everytime the view has been checked.
+    console.log('ngAfterViewChecked called!');    
+  }
+
+  ngOnDestroy(){ //Called when something is removed from the DOM, in the example we removed the element of the array, so when page load it doesn't find it.
+    console.log('ngOnDestroy called!');    
   }
 
 }
